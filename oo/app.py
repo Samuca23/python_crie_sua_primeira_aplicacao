bfafb13656616544e39814f75aefccba5c66e13a
Samuel <samuel.chiodini@unidavi.edu.br>
2024-02-26 13:32:10 -0300

    modulo 2

--- oo/app.py
+++ oo/app.py
@@ -4,7 +4,9 @@ from model.cardapio.prato import Prato
 
 restaurante_1 = Restaurante('Praça', 'Gourmet')
 bebida = Bebida('Suco de Menlancia', 5.0, 'grande')
+bebida.aplicar_desconto()
 prato = Prato('Pãozinho', 4.0, 'O melhor pão da cidade')
+prato.aplicar_desconto()
 restaurante_1.adicionar_no_cadarpio(bebida)
 restaurante_1.adicionar_no_cadarpio(prato)
 
